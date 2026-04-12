0c9d7c843ea74bc365e4b87223a3797bfb62bd07
D Chinmay <zchinmayz@gmail.com>
2026-04-13 00:19:26 +0530

    jumpgame1

---
+++ 15. Greedy/jumpgame1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+// we can jump to any index which is <= arr[i]
+// ex 2 3 1 1 4, at index 0:a[0]=2 so we can jump either 1 or 2 times
+// we will track the max index we can jump to if we encounter a index which is greater than maxindex thats not possble hence false
+
+public class jumpgame1 {
+
+    public static boolean canJump(int[] nums) {
+        int n = nums.length;
+        int mxInd = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(mxInd < i) return false;
+            mxInd = Math.max(mxInd, i+nums[i]);
+            if(mxInd >= n-1) return true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 1, 4};
+         boolean result = canJump(arr);
+
+        System.out.println(result);
+    }
+}
